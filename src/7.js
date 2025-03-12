aaedb555fbddd5302b257591c9b055a43546667c
julsrudshipmen775 <julsrudshipmen775@gmail.com>
2025-03-12 11:13:23 +0000

    Add 7.js

---
+++ src/7.js
@@ -0,0 +1,8 @@
+const generateRandomCode = () => {
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let code = '';
+  for (let i = 0; i < 10; i++) {
+    code += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return code;
+}
