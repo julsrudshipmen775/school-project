af8cdb738d902513c58fada640d9477b2d71a4a4
julsrudshipmen775 <julsrudshipmen775@gmail.com>
2025-03-17 06:02:22 +0000

    Add 12.js

---
+++ src/12.js
@@ -0,0 +1,9 @@
+  <script>
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * max);
+    }
+
+    const randomNumber = getRandomInt(10);
+
+    console.log(`This is a simple school project repository for sharing project files and collaborating with classmates.`);
+  </script>
