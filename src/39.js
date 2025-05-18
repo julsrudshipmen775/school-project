1b11f6daa121619e699b42a44ef110e94c98f541
julsrudshipmen775 <julsrudshipmen775@gmail.com>
2025-05-18 09:02:33 +0000

    Add 39.js

---
+++ src/39.js
@@ -0,0 +1,12 @@
+function addNumber(num1, num2) {
+  const result = num1 + num2;
+  console.log(result);
+}
+
+addNumber(5, 3);
+
+function displayMessage(message) {
+  console.log(`Your message: ${message}`);
+}
+
+displayMessage("Hello, World!");
